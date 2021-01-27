dc45d8c6b2e22f49dc995fa4743087e22fe71463
Dylan <dcren2010@gmail.com>
2021-01-27 16:31:52 -0500

    dotenv installed

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/postsRoute.js'
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: '30mb', extended: true }));
@@ -16,9 +18,9 @@ app.get('/', (req, res) => res.send('API is running successfully...'));
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://user01:user01@cluster0.lxcvk.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://user01:user01@cluster0.lxcvk.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server is running on port ${ PORT }`)))
   .catch(err => console.log(err.message));
 
